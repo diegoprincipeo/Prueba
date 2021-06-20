e38370205ae7eb79971147caafc72cb36492661e
Diego Principe <diegoprincipeo@gmail.com>
2021-06-20 00:15:53 -0500

    Funciones

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
-for (let i=0; i<1; i++) {
-    let producto = parseInt(prompt("Ingrese el producto que desea comprar"));
-    switch (producto) {
-        case 1:
-            console.log("Aceite Primor");
-            break;
-        case 2:
-            console.log("Arroz Costeño");
-            break;
-        case 3:
-            console.log("Leche Gloria");
-            break;
-        case 4:
-            console.log("Aceite Sao");
-            break;
-        default:
-            console.log("El Codigo no existe, favor de ingresar un numero del 1 al 4");
-            break;
-    }
-}
+ const Total = () => {
+     alert(("El monto a pagar por tus productos es: S/. ") + ((producto1 + producto2)*1.18));
+ }
+ var producto1=parseInt(prompt("Ingresa el valor del primer producto"));
+ var producto2=parseInt(prompt("Ingreas el valor del segundo producto"));
+ Total()
+ alert("Felicidades!!! eres nuestro cliente numero 1,000,000 ganaste un descuento del 10% en tu compra")
+ var Descuento = ((producto1+producto2)*1.18*0.9)
+ alert (("Por lo cual tus productos solo te costaran: S/. ") + Descuento)
+ alert("Y el envio te saldra S/ 5.00")
+ var Delivery = (((producto1+producto2)*1.18*0.9)+ 5)
+ alert (("Por lo que el total de tu compra es: S/. ") + Delivery)
