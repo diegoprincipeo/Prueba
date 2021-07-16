1db9406d7e574a74f657c30f6fed2a321351467a
Diego Principe <diegoprincipeo@gmail.com>
2021-07-15 22:45:01 -0500

    722

---
+++ JavaScript_Curso/js/carrito.js
@@ -0,0 +1,90 @@
+//Añadir producto al carrito//
+class Carrito {
+    comprarProducto(e){
+        e.preventDefault();
+        if(e.target.classList.contains('agregar-carrito')){
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatosProducto(producto);
+        }
+    }
+    //leer datos de productos//
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen : producto.querySelector('img').src,
+            titulo : producto.querySelector('h3').textContent,
+            precio : producto.querySelector('.precio span').textContent,
+            id: producto.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
+        }
+        this.insertarCarrito(infoProducto);
+    }
+    //Muestra datos de productos//
+    insertarCarrito(producto){
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${producto.imagen}" width=100>
+            </td>
+            <td>${producto.titulo}</td>
+            <td>${producto.precio}</td>
+            <td>
+                <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+            </td>
+        `;
+        listaProductos.appendChild(row);
+        this.guardarProductosLocalStorage(producto);
+    }
+    //Elimina un producto//
+    eliminarProducto(e){
+        e.preventDefault();
+        let producto, productoID;
+        if(e.target.classList.contains('borrar-producto')){
+            e.target.parentElement.parentElement.remove();
+            producto = e.target.parentElement.parentElement;
+            productoID = producto.querySelector('a').getAttribute('data-id');
+        }
+        this.eliminarProductoLocalStorage(productoID);
+    }
+    //Elimina todos los productos//
+    vaciarCarrito(e){
+        e.preventDefault();
+        while(listaProductos.firstChild){
+            listaProductos.removeChild(listaProductos.firstChild);
+        }
+        return false;
+    }
+    //Guardar en el LocalStorage//
+    guardarProductosLocalStorage(producto){
+        let productos;
+        productos = this.obtenerProductosLocalStorage();
+        productos.push(producto);
+        localStorage.setItem('productos', JSON.stringify(productos));
+    }
+        //Comprueba datos LocalStorage//
+    obtenerProductosLocalStorage(){
+        let productoLS;
+        if(localStorage.getItem('productos') === null){
+            productoLS = [];
+        }
+        else {
+            productoLS = JSON.parse(localStorage.getItem('productos'));
+        }
+        return productoLS;
+    }
+    
+        //Eliminar producto por ID del LS
+    eliminarProductoLocalStorage(productoID){
+        let productosLS;
+        //Obtenemos el arreglo de productos
+        productosLS = this.obtenerProductosLocalStorage();
+        //Comparar el id del producto borrado con LS
+        productosLS.forEach(function(productoLS, index){
+            if(productoLS.id === productoID){
+                productosLS.splice(index, 1);
+            }
+        });
+
+        //Añadimos el arreglo actual al LS
+        localStorage.setItem('productos', JSON.stringify(productosLS));
+    }
+}
